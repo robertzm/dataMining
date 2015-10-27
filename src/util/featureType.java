a769f550b44209b125cacf37d9b342df6860c5bf
robertzm <zibo.meng@emory.edu>
2015-10-27 04:02:28 -0400

    temp

---
+++ src/util/featureType.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * Created by robertmeng on 10/22/15.
+ *
+ * @Author robert Z. Meng
+ * @mail zibo.meng@emory.edu
+ * @Description
+ */
+public enum featureType
+{
+    NOMINAL,
+    NUMERICAL
+}
