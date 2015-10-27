c8aabb49eae24d5e567914dffc57971827f2d318
robertzm <zibo.meng@emory.edu>
2015-10-27 04:01:04 -0400

    arff reader

---
+++ src/reader/arffReader.java
@@ -0,0 +1,11 @@
+package reader;
+
+/**
+ * Created by robertmeng on 10/27/15.
+ *
+ * @Author robert Z. Meng
+ * @mail zibo.meng@emory.edu
+ * @Description
+ */
+public class arffReader {
+}
---
+++ src/tester/tester.java
@@ -0,0 +1,11 @@
+package tester;
+
+/**
+ * Created by robertmeng on 10/27/15.
+ *
+ * @Author robert Z. Meng
+ * @mail zibo.meng@emory.edu
+ * @Description
+ */
+public class tester {
+}
