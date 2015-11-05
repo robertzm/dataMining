5dae9c6b56aedfcbbeb05f9e90297371470ac411
robertzm <zibo.meng@emory.edu>
2015-11-05 11:22:00 -0500

    update tester

--- src/tester/tester.java
+++ src/tester/tester.java
@@ -4,11 +4,9 @@ import cluster.KMeans;
 import com.beust.jcommander.JCommander;
 import reader.arffReader;
 import reader.paraReader;
-import reader.writer;
 import util.instances;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
 
 /**
  * Created by robertmeng on 10/27/15.
@@ -20,7 +18,8 @@ import java.util.ArrayList;
 public class tester {
     public static void main(String[] args) throws IOException {
         paraReader jct = new paraReader();
-        String[] argv1 = {"-i", "dat/baskball.arff", "-o", "output.txt", "-sr", "2", "-er", "30", "-iter", "100"};
+        String[] argv1 = {"-i", "dat/baskball.arff", "-o", "output.txt", "-sr", "2", "-er", "20", "-iter", "100", "-n", "0"};
+        //String[] argv1 = {"-i", "dat/cloud.arff", "-o", "output.txt", "-sr", "2", "-er", "10", "-iter", "100", "-n", "false"};
         new JCommander(jct, argv1);
 
         instances insts = new instances();
@@ -29,43 +28,28 @@ public class tester {
         rdr.readTrnInst();
         rdr.close();
 
-        writer output = new writer(jct.outDir, "utf-8");
+        Writer output =new BufferedWriter(new OutputStreamWriter(new FileOutputStream(jct.outDir), "utf-8"));
 
-        int bestK = 0;
-        double bestSSE = Double.MAX_VALUE;
-        ArrayList<double[]> bestCenters;
         int start = jct.startRange;
 
-        if(insts.isAllNumeric()){
-            while( start < jct.endRange) {
-                int iteration = jct.iter;
-                double tmpBestSSE = Double.MAX_VALUE;
-                ArrayList<double[]> tmpBestCenters = new ArrayList<>();
+        while( start < jct.endRange) {
+            int iteration = jct.iter;
+            KMeans bestKM = new KMeans(start, insts, jct.norm == 1);
+            bestKM.SSE = Double.MAX_VALUE;
 
-                while(iteration-- > 0) {
-                    KMeans km = new KMeans(start, insts);
-                    km.train();
-                    if(km.sqDistSum < tmpBestSSE){
-                        tmpBestSSE = km.sqDistSum;
-                        tmpBestCenters = new ArrayList<>();
-                        for(int i = 0; i < km.centers.size(); i++) {
-                            tmpBestCenters.add(km.centers.get(i).center.inst);
-                        }
-                    }
+            while(iteration-- > 0) {
+                KMeans km = new KMeans(start, insts, jct.norm == 1);
+                km.train();
+                if(km.SSE < bestKM.SSE){
+                    bestKM = km;
                 }
-                System.out.println(start);
-                System.out.println(tmpBestSSE);
-
-                if(tmpBestSSE < bestSSE){
-                    bestSSE = tmpBestSSE;
-                    bestCenters = (ArrayList<double[]>) tmpBestCenters.clone();
-                    bestK = start;
-                }
-                start ++;
             }
-        }else{
-            System.out.println("ERROR: not all features numerical");
-            System.exit(0);
+            bestKM.printInfo();
+            bestKM.output(output);
+            start ++;
         }
+
+        output.close();
+
     }
 }
