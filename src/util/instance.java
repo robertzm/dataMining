a36c523c6ac54ce7a697332467ff5c2e1c36c394
robertzm <zibo.meng@emory.edu>
2015-11-05 11:21:34 -0500

    add norminal distance calc and normalization for numeric features

--- src/util/instance.java
+++ src/util/instance.java
@@ -1,5 +1,7 @@
 package util;
 
+import filter.Normalization;
+
 /**
  * Created by robertmeng on 10/27/15.
  *
@@ -14,14 +16,20 @@ public class instance {
         inst = new double[n];
     }
 
-    public double sqDist(instance center){
+    public double sqDist(instance center, instances insts, boolean norm){
         double rst = 0;
         if(this.inst.length != center.inst.length){
             System.out.println("ERROR: instance length not match");
             System.exit(0);
         }
         for(int i = 0; i < this.inst.length; i ++){
-            rst += Math.pow((this.inst[i] - center.inst[i]), 2);
+            if (insts.getFeatures().get(i).isNumeric())
+                if (!norm)
+                    rst += Math.pow((this.inst[i] - center.inst[i]), 2);
+                else
+                    rst += Math.pow((Normalization.norm(this.inst[i], insts.getFeatures().get(i).getMinValue(), insts.getFeatures().get(i).getMaxValue(), 0, 1)
+                            - Normalization.norm(center.inst[i], insts.getFeatures().get(i).getMinValue(), insts.getFeatures().get(i).getMaxValue(), 0, 1)), 2);
+            else rst += this.inst[i] == center.inst[i]? 0: 1;
         }
         return rst;
     }
